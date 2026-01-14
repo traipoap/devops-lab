c584a2016020967ac806836b7a9707ea2422375f
traipoap <traipoap@hotmail.com>
2026-01-14 10:22:04 +0700

    Update main.tf

--- terraform-provider-proxmox/main.tf
+++ terraform-provider-proxmox/main.tf
@@ -1,7 +1,10 @@
 terraform {
+  required_version = ">= 0.13.0"
+
   required_providers {
     proxmox = {
       source = "Telmate/proxmox"
+      version = "3.0.2-rc07"
     }
   }
 
@@ -13,30 +16,59 @@ terraform {
   }
 }
 
-provider "proxmox" {
-  pm_api_url = "https://192.168.1.2:8006/api2/json"
-  pm_api_token_id="root@pam!terraform"
-  pm_api_token_secret="03f80266-0cce-48cf-a40f-6b266f9d154a"
-  pm_tls_insecure = true
+variable "proxmox_api_url" {
+  type = string
 }
+variable "proxmox_api_token_id" {
+  type = string
+  sensitive = true
+}
+variable "proxmox_api_token_secret" {
+  type = string
+  sensitive = true
+}
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
 
-resource "proxmox_vm_qemu" "debian-13" {
-  name        = "debian-13"
+  pm_tls_insecure = true
+  pm_debug = true
+}
+resource "proxmox_vm_qemu" "terraform-vm" {
+  force_create= true
+  vmid        = 105
+  name        = "terraform-vm"
   target_node = "local"
-  iso         = "debian-13.1.0-amd64-netinst.iso"
-  cores       = 4
-  memory      = 4096
+  
+  clone       = "ubuntu-24.04.2"
+  full_clone  = false
+  os_type     = "cloud-init"
+  
+  cores       = 1
+  agent       = 0
+  memory      = 1024
+  scsihw      = "virtio-scsi-single"
+  boot        = "order=scsi0"
+
+  # Cloud-Init configuration
+  nameserver = "1.1.1.1 8.8.8.8"
+  ipconfig0  = "ip=192.168.1.10/24,gw=192.168.1.1"
+  ciuser     = "traipoap"
+  cipassword = "32110"
+  sshkeys    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/Pjg7YXZ8Yau9heCc4YWxFlzhThnI+IhUx2hLJRxYE Cloud-Init@Terraform"
 
   disk {
-    size = "32G"
-    type = "scsi"
+    slot    = "scsi0"
+    size    = "32G"
+    type    = "disk"
     storage = "st500"
-    discard = "on"
+    discard = false
   }
+
   network {
-    model = "virtio"
+    id = 0
     bridge = "vmbr0"
-    firewall = false
-    link_down = false
+    model  = "e1000"
   }
 }
